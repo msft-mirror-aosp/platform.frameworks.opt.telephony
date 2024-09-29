4afd6be946976b428af35975aae76a66ef52bda7
arunvoddu <arunvoddu@google.com>
2024-09-29 16:14:33 +0000

    adb shell command to trigger phone.notifyCarrierRoamingNtnEligibleStateChanged API.
    adb shell cmd phone set-satellite-access-restriction-checking-result true
    
    Flag: EXEMPT bugfix
    Bug: 370033218
    Test: Manually verified.
    Change-Id: I5a3cf3769bb2a56706367c6b0307118bd5adecbe

--- src/java/com/android/internal/telephony/satellite/SatelliteController.java
+++ src/java/com/android/internal/telephony/satellite/SatelliteController.java
@@ -573,6 +573,7 @@ public class SatelliteController extends Handler {
     private static final String HOW_IT_WORKS_BUTTON = "how_it_works_button";
     private static final String ACTION_NOTIFICATION_CLICK = "action_notification_click";
     private static final String ACTION_NOTIFICATION_DISMISS = "action_notification_dismiss";
+    private AtomicBoolean mOverrideNtnEligibility;
     private BroadcastReceiver
             mDefaultSmsSubscriptionChangedBroadcastReceiver = new BroadcastReceiver() {
                 @Override
@@ -5347,6 +5348,11 @@ public class SatelliteController extends Handler {
             return;
         }
 
+        if (mOverrideNtnEligibility != null) {
+            mSatellitePhone.notifyCarrierRoamingNtnEligibleStateChanged(currentNtnEligibility);
+            return;
+        }
+
         synchronized (mSatellitePhoneLock) {
             if (mSatellitePhone == null) {
                 ploge("notifyNtnEligibility: mSatellitePhone is null");
@@ -6516,6 +6522,11 @@ public class SatelliteController extends Handler {
             return false;
         }
 
+        if (mOverrideNtnEligibility != null) {
+            // TODO need to send the value from `mOverrideNtnEligibility` or simply true ?
+            return true;
+        }
+
         if (SatelliteServiceUtils.isCellularAvailable()) {
             plogd("isCarrierRoamingNtnEligible[phoneId=" + phone.getPhoneId()
                     + "]: cellular is available");
@@ -6744,5 +6755,30 @@ public class SatelliteController extends Handler {
         synchronized (mSatelliteEnabledRequestLock) {
             return !mWaitingForSatelliteModemOff;
         }
+    }
+
+    /**
+     * Method to override the Carrier roaming Non-terrestrial network eligibility check
+     *
+     * @param state         flag to enable or disable the Ntn eligibility check.
+     * @param resetRequired reset overriding the check with adb command.
+     */
+    public boolean overrideCarrierRoamingNtnEligibilityChanged(boolean state,
+            boolean resetRequired) {
+        Log.d(TAG, "overrideCarrierRoamingNtnEligibilityChanged state = " + state
+                + "  resetRequired = " + resetRequired);
+        if (resetRequired) {
+            mOverrideNtnEligibility = null;
+        } else {
+            if (mOverrideNtnEligibility == null) {
+                mOverrideNtnEligibility = new AtomicBoolean(state);
+            } else {
+                mOverrideNtnEligibility.set(state);
+            }
+            if (this.mSatellitePhone != null) {
+                updateLastNotifiedNtnEligibilityAndNotify(state);
+            }
+        }
+        return true;
     }
 }
