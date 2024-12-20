aa94fa6ef0189e4c20875e783c64addc8608dbaf
Daniel Banta <danielbanta@google.com>
2024-12-20 11:16:40 -0800

    Allow GsmSMSDispatcher to send MtSmsPollingMessage while not in service.
    The CL includes:
    - Remove the state in-service check for MT SMS poll messages GsmSMSDispatcher.
    - Fix the logic for mShouldPollMtSms to allow retry sending check message if sending fails.
    
    
    Bug: 385011293
    Test: atest GsmSmsDispatcherTest, DatagramDispatcherTest
    FLAG: com.android.internal.telephony.flags.carrier_roaming_nb_iot_ntn
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:99126fdf5fd8623287f0d9b381b5f78355b8a8e4)
    Merged-In: I6f685138fd15db486ac6341b39be457949e6305d
    Change-Id: I6f685138fd15db486ac6341b39be457949e6305d

--- src/java/com/android/internal/telephony/satellite/DatagramDispatcher.java
+++ src/java/com/android/internal/telephony/satellite/DatagramDispatcher.java
@@ -1254,6 +1254,7 @@ public class DatagramDispatcher extends Handler {
                         getPendingMessagesCount(), SATELLITE_RESULT_SUCCESS);
                 if (datagramType == DATAGRAM_TYPE_CHECK_PENDING_INCOMING_SMS) {
                     startMtSmsPollingThrottle();
+                    mShouldPollMtSms = false;
                 }
             } else {
                 // Update send status
@@ -1295,7 +1296,9 @@ public class DatagramDispatcher extends Handler {
         }
 
         plogd("sendMtSmsPollingMessage");
-        mShouldPollMtSms = false;
+        if (!allowCheckMessageInNotConnected()) {
+            mShouldPollMtSms = false;
+        }
 
         for (Entry<Long, PendingRequest> entry : mPendingSmsMap.entrySet()) {
             PendingRequest pendingRequest = entry.getValue();
